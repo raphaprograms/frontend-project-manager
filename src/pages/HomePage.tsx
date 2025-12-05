6bccc623056cf9a24c88387bd560693d837343bf
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-12-05 11:57:02 -0800

    Make homepage test changed

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,9 +2,14 @@
 
 function HomePage() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-white">Project Manager App</h1>
-    </div>
+    <>
+      <div>
+        <h1 className="text-4xl font-bold text-white">Project Manager App</h1>
+      </div>
+      <div>
+        <h2>Testing</h2>
+      </div>
+    </>
   );
 }
 export default HomePage;
