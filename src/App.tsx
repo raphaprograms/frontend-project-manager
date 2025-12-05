14b491f8f9701c615d3eefd176aec6e7359cc5c2
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-12-05 10:17:55 -0800

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProjectsPage from './pages/ProjectsPage';
+import ProjectDetailsPage from './pages/ProjectDetailsPage';
+import Navbar from './components/Navbar';
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='bg-zinc-900 h-screen'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/projects' element={<ProjectsPage />}/>
+        <Route path='/projects/:projectId' element={<ProjectDetailsPage />}/>
+        
+      </Routes>
+    </div>
+    </>
+  )
+}
+
+export default App;
---
+++ src/clients/api.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+    baseURL: 'http://localhost:4000',
+    headers: {
+        Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzBjZTUyNWZmYTNjYjRjY2Q2MGYxNCIsInVzZXJuYW1lIjoiVXNlcjEiLCJlbWFpbCI6InVzZXIxQHRlc3QuY29tIiwicm9sZSI6InVzZXIifSwiaWF0IjoxNzY0OTU2NTUyLCJleHAiOjE3NjQ5NjM3NTJ9.WAZkVjoP-Xv_ZvmSdeQb2UKJ-YRq5NM5sIfMzif8QGc'
+    }
+});
+
+export default apiClient;
