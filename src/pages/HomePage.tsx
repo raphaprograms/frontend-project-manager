502a64486b66ed4b72d1c47e14db64c24f6ff63b
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-12-05 11:59:24 -0800

    Make homepage test changed

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,7 +7,7 @@ function HomePage() {
         <h1 className="text-4xl font-bold text-white">Project Manager App</h1>
       </div>
       <div>
-        <h2>Testing</h2>
+        <h2 className="text-3xl font-bold text-white">Testing</h2>
       </div>
     </>
   );
