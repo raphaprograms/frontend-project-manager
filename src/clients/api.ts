177d9d667a2f1657be4b46f267525d6a553a4764
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-12-08 12:55:53 -0800

    run build

--- src/clients/api.ts
+++ src/clients/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiClient = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
     headers: {
-        Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzBjZTUyNWZmYTNjYjRjY2Q2MGYxNCIsInVzZXJuYW1lIjoiVXNlcjEiLCJlbWFpbCI6InVzZXIxQHRlc3QuY29tIiwicm9sZSI6InVzZXIifSwiaWF0IjoxNzY1MjIxMzUyLCJleHAiOjE3NjUzMDc3NTJ9.Ir6C1GCOxlSYW_AAIqqxUi5A7m8_d2Bqz2w6KYSZbL0'
+        Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzBjZTUyNWZmYTNjYjRjY2Q2MGYxNCIsInVzZXJuYW1lIjoiVXNlcjEiLCJlbWFpbCI6InVzZXIxQHRlc3QuY29tIiwicm9sZSI6InVzZXIifSwiaWF0IjoxNzY1MjI0MTIyLCJleHAiOjE3NjUzMTA1MjJ9.OtndeueqLSCR3IpKCnn--PGhy1rhYEODP05oyokxXdc'
     }
 });
 
